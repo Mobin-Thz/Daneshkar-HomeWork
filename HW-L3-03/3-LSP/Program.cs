a90919e3638167d87571a43d27478760cd61df64
mobin-thz <mobintaherzadeh@gmail.com>
2025-05-02 00:09:39 +0330

    HW-L3-03-03

--- HW-L3-03/3-LSP/Program.cs
+++ HW-L3-03/3-LSP/Program.cs
@@ -9,6 +9,86 @@
         static void Main(string[] args)
         {
         }
+
+
+        //Wrong Approach
+
+
+
+
+        //class Bird
+        //{
+        //    void Fly() { }
+
+        //    void Swim() { }
+
+        //}
+
+        //class Penguin:Bird
+        //{
+        //    void Fly()
+        //    {
+        //        Console.WriteLine("Penguin cannot fly!");
+        //    }
+
+        //    void Swim()
+        //    {
+        //        Console.WriteLine("Penguin swims");
+        //    }
+        //}
+
+        //class Eagle: Bird
+        //{
+        //    void Fly()
+        //    {
+        //        Console.WriteLine("Eagle flies");
+        //    }
+
+        //    void Swim()
+        //    {
+        //        Console.WriteLine("Eagle cannot swim!");
+        //    }
+        //}
+
+        //پس فرزندان نمیتوانند جایگزین پدر خود شوند چون تمام ویژگی های ان را ندارند 
+
+
+
+
+
+
+        //------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+        // Correct Approach 
+
+        interface IFlyingBird
+        {
+            void Fly();
+        }
+
+        interface ISwimmingBird
+        {
+            void Swim();
+        }
+
+        class Eagle : IFlyingBird
+        {
+            public void Fly()
+            {
+                Console.WriteLine("Eagle Flies");
+            }
+        }
+
+        class Penguin : ISwimmingBird
+        {
+            public void Swim()
+            {
+                Console.WriteLine("Penguin Swimss");
+            }
+        }
     }
 
 
