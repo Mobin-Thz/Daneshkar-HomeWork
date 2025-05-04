c5a105d19e2291b04f5f2207b5b90fb884133c67
mobin-thz <mobintaherzadeh@gmail.com>
2025-05-05 00:57:46 +0330

    HW-L3-03-06

---
+++ HW-L3-03/5-DIP/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _5_DIP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+        //این اصل میگوید سیستم های نرم افزاری زمانی انعطاف پذیر و کم هزینه تر برای تغییر هستند
+        //که وابستگی های کد به جای جزئیات پیاده سازی کلاس ها یا ماژولهای عینی  به مفاهیم کلی و انتزاعی اینترفیس ها یا کلاس های انتزاع  اشاره کنند
+
+        //Wrong 
+
+        //public class SQLUserRepo
+        //{
+        //    public void Save() { }
+
+        //}
+
+        //public class User
+        //{
+        //    SQLUserRepo SQLUserRepo = new SQLUserRepo();
+
+        //    void SaveUserToDatabase()
+        //    {
+        //        SQLUserRepo.Save();
+        //    }
+
+        //}
+
+        //---------------------------------------------------------------------------------------------
+        //Correct approach
+
+        interface IUserRepository
+        {
+            public void Save();
+        }
+
+        class SQLUserRepo : IUserRepository
+        {
+            public void Save()
+            {
+                Console.WriteLine("User saved into repository");
+            }
+        }
+
+        class User
+        {
+            private IUserRepository userRepository;
+
+            public User(IUserRepository userRepository)
+            {
+                this.userRepository = userRepository;
+            }
+
+            void SaveUserToDatabase()
+            {
+                userRepository.Save();
+            }
+        }
+
+    }
+}
