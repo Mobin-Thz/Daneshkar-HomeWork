1f7ae3b1c97a01e3b8f2c684cb096fcf606f789a
mobin-thz <mobintaherzadeh@gmail.com>
2025-06-22 22:19:33 +0330

    HW-L5-01-03

---
+++ HW-L5-01/2-IsPrime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPrime(int n)
+    {
+        if (n < 2)
+            return false;
+        for (int i = 2; i * i <= n; i++)
+            if (n % i == 0)
+                return false;
+        return true;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter n: ");
+        if (int.TryParse(Console.ReadLine(), out int n))
+            Console.WriteLine(IsPrime(n) ? "Prime" : "Composite");
+    }
+}
---
+++ HW-L5-01/3-LongestIncreasingSubarray/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _3_LongestIncreasingSubarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(LongestIncreasingSubarray(new[] { 1, 2, 2, 3, 4, 1, 2 }));  // 3 ([2,3,4])
+            Console.WriteLine(LongestIncreasingSubarray(new[] { 5, 6, 7, 1, 2, 3, 4 }));  // 4 ([1,2,3,4])
+            Console.WriteLine(LongestIncreasingSubarray(new[] { 9, 8, 7 }));              // 1
+
+        }
+
+        static int LongestIncreasingSubarray(int[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+                return 0;
+
+            int maxLen = 1;
+            int currLen = 1;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > arr[i - 1])
+                {
+                    currLen++;
+                    if (currLen > maxLen)
+                        maxLen = currLen;
+                }
+                else
+                {
+                    currLen = 1;
+                }
+            }
+
+            return maxLen;
+        }
+
+    }
+}
