fdc52531f6bd772006906b9825a278f843f26726
mobin-thz <mobintaherzadeh@gmail.com>
2025-08-17 15:49:26 +0330

    HW_L5_BookStore(Structure)

---
+++ HW-L5-02/BookStore/BookStore.Domain/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookStore.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
